a3c1819b9889262ecc9ae6878e49a00c9dcd4971
mitchell.liversage <mitchellliversage@gmail.com>
2018-05-01 17:48:39 +1000

    Created Visual Studio Program

---
+++ unity/Assets/~GoneHome/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public string message = "Hello World";
+    public Rigidbody rigid;
+    float speed = 5;
+    // Use this for initialization
+    void Start()
+    {
+        print("Hello World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rigid.AddForce(Vector3.forward * speed);
+        }
+
+
+    }
+}
+
